2f02498eee1eddd242939f3ffc51fdb5d66935d7
鈴木雄大 <user@YSMacBook.local>
2025-05-07 18:34:25 +0900

    完成！

---
+++ src/components/layouts/Home/index.js
@@ -0,0 +1,11 @@
+export { default as Voice } from './Voice.astro';
+export { default as Profile } from './Profile.astro';
+export { default as Worries } from './Worries.astro';
+export { default as Feature } from './Feature.astro';
+export { default as Members } from './Members.astro';
+export { default as Price } from './Price.astro';
+export { default as Cta } from './Cta.astro';
+export { default as Flow } from './Flow.astro';
+export { default as Qa } from './Qa.astro';
+export { default as Access } from './Access.astro';
+
